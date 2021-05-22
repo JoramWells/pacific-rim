1fb7c7acdf0f86748f6254b0a65ccb4e26dc5a03
unknown <jorammanoah1@gmail.com>
2021-05-22 00:08:09 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div style={{backgroundColor:"#002651", height:"100vh", textAlign:"center",margin:"0"}}>
+      <header style={{padding:"3rem"}}>
+        <h1 style={{color:"white", margin:"0"}}>Pacific</h1>
+
       </header>
     </div>
+
   );
 }
 
